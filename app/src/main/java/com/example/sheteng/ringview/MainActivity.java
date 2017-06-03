85a67ea889bd8e6942edfe1c3d5cd3b27a515cac
sheteng <sheteng@sina.cn>
2017-06-03 17:44:37 +0800

    ringview

--- app/src/main/java/com/example/sheteng/ringview/MainActivity.java
+++ app/src/main/java/com/example/sheteng/ringview/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.sheteng.ringview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RingView mRingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRingView = (RingView) findViewById(R.id.ringview);
     }
+
+    public void showRing(){
+        ArrayList<Float> acrs = new ArrayList<>();
+        acrs.add(5f);
+        acrs.add(4f);
+        acrs.add(7f);
+        acrs.add(8f);
+        mRingView.initData(acrs);
+    }
+
 }
---
+++ app/src/main/java/com/example/sheteng/ringview/RingView.java
@@ -0,0 +1,126 @@
+package com.example.sheteng.ringview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sheteng on 2017/5/9.
+ */
+
+public class RingView extends View {
+
+    private static final String TAG = "RingView";
+    private Paint mPaint;
+    private Context mContext;
+    private float mWidth;
+    private float mHeight;
+    private float mOuterRing;
+    private int[] colors = new int[]{0xFFF9A947, 0xFFe57646, 0xFF1E71BC, 0xFF428BFE};
+    private List<Float> mListAcr;
+    //内圈大小
+    private float mSmallAcr;
+    private float mStartAcr = 1f;
+    private RectF mRectF;
+    private float mInnerRing;
+
+    public RingView(Context context) {
+        super(context, null, 0);
+    }
+
+    public RingView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RingView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mListAcr = new ArrayList<>();
+        mContext = context;
+        mPaint = new Paint();
+        mPaint.setDither(true);
+        mPaint.setAntiAlias(true);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.RingView);
+        mOuterRing = typedArray.getDimension(R.styleable.RingView_outer_ring_radius, 0);
+        mInnerRing = typedArray.getDimension(R.styleable.RingView_inner_ring_radius, 0);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidth = getMeasuredWidth();
+        mHeight = getMeasuredHeight();
+        float left = (mWidth - mOuterRing) / 2;
+        float top = (mHeight - mOuterRing) / 2;
+        mRectF = new RectF(left, top, mWidth - left, mHeight - top);
+    }
+
+    public void initData(List<Float> list) {
+        mListAcr.clear();
+        if (list == null) {
+            return;
+        }
+        float total = 0;
+        for (Float integer : list) {
+            total += integer;
+        }
+        if (total > 0) {
+            float totalAcr = 0;
+            for (int i = 0; i < list.size(); i++) {
+                float acr = (list.get(i) / total) * 360;
+                if (i == list.size() - 1) {
+                    acr = 360 - totalAcr;
+                }
+                totalAcr += acr;
+                mListAcr.add(acr);
+            }
+
+        }
+        mStartAcr = 1;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int total = 0;
+        mStartAcr += getInterpolation(mStartAcr / 360f);
+        if (mStartAcr > 360) {
+            mStartAcr = 360;
+        }
+        for (int i = 0; i < mListAcr.size(); i++) {
+            mPaint.setColor(colors[i]);
+            if (mListAcr.get(i) == 0) {
+                continue;
+            }
+            float endAcr = mStartAcr - total;
+            canvas.drawArc(mRectF, 270 + total, endAcr, true, mPaint);
+            if (mStartAcr >= total + mListAcr.get(i)) {
+                total += mListAcr.get(i);
+                continue;
+            } else {
+                break;
+            }
+        }
+        mPaint.setColor(Color.WHITE);
+        canvas.drawCircle(mWidth / 2, mHeight / 2, mInnerRing / 2, mPaint);
+        if (mStartAcr < 360) {
+            invalidate();
+        }
+    }
+
+    /**
+     * 生成插值
+     */
+    public float getInterpolation(float input) {
+        double cos = Math.cos(input * Math.PI);
+        return (float) ((1f - Math.abs(cos)) * 6 + 3f);
+    }
+}
